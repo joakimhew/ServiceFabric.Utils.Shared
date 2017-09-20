fd6385916c7b3546c6a9ee9defb7e390a74d3720
Joakim Hansson <joakim.hansson@tretton37.com>
2017-09-20 14:51:45 +0200

    Port main functionality to .NET core

---
+++ ApiResponseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ServiceFabric.Utils.Shared.Core
+{
+    public class ApiResponseMessage<TMessageType>
+    {
+        public HttpStatusCode Code { get; set; }
+        public TMessageType Message { get; set; }   
+        public string Info { get; set; }
+    }
+}
---
+++ Extensions/HttpContentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabric.Utils.Shared.Core.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        /// <summary>
+        /// Reads <see cref="HttpContent"/> as an <see cref="ApiResponseMessage{TMessageType}"/> 
+        /// with <see cref="ApiResponseMessage{TMessageType}.Message"/> set to <typeparam name="TExpectedMessageType"/> 
+        /// as a synchronous operation
+        /// </summary>
+        /// <typeparam name="TExpectedMessageType">Expected type of the <see cref="ApiResponseMessage{TMessageType}.Message"/> 
+        /// property in <see cref="HttpContent"/></typeparam>
+        /// <param name="content"><see cref="HttpContent"/> to read from and deserialize</param>
+        /// <returns><see cref="ApiResponseMessage{TMessageType}"/> with the <see cref="ApiResponseMessage{TMessageType}.Message"/> 
+        /// property set to <typeparam name="TExpectedMessageType"/></returns>
+        public static async Task<ApiResponseMessage<TExpectedMessageType>> ReadAsApiResponseMessageAsync<TExpectedMessageType>(this HttpContent content)
+        {
+            var json = await content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ApiResponseMessage<TExpectedMessageType>>(json);
+        }
+    }
+}
