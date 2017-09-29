9fd10a07630407d8ddd00b82a92109e5d59eab60
Joakim Hansson <joakim.hansson@tretton37.com>
2017-09-29 15:21:17 +0200

    Set namespace for extensions to a more friendly name

--- NETSTANDARD2_0/Extensions/HttpClientExtensions.cs
+++ NETSTANDARD2_0/Extensions/HttpClientExtensions.cs
@@ -2,12 +2,11 @@
 
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ServiceFabric.Utils.Shared.NETSTANDARD2_0.Extensions
+namespace ServiceFabric.Utils.Shared.Extensions
 {
     public static class HttpClientExtensions
     {
